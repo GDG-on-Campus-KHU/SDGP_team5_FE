9942ad2f60caf495882c2b0c091bb7ad439003d1
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-04-09 23:41:49 +0900

    Add TopBar

---
+++ app/src/main/java/com/example/resq/presentaion/component/TopBar.kt
@@ -0,0 +1,25 @@
+package com.example.resq.presentaion.component
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar() {
+    TopAppBar(
+        title = {
+            Text(text = "ResQ")
+        },
+        actions = {
+            IconButton(onClick = { /* Search Action */ }) {
+                Icon(Icons.Default.Search, contentDescription = "Search")
+            }
+        },
+    )
+}
