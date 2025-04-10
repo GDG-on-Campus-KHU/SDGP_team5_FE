b22ab602facd0c280c217287b5ff4833c04a1404
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-04-11 00:17:54 +0900

    Add TopBar navigationIcon

--- app/src/main/java/com/example/resq/presentaion/component/TopBar.kt
+++ app/src/main/java/com/example/resq/presentaion/component/TopBar.kt
@@ -1,25 +1,56 @@
 package com.example.resq.presentaion.component
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.resq.navigation.home.HomeNavigationItem
+import com.example.resq.navigation.share.ShareNavigationItem
+import com.example.resq.navigation.user.UserNavigationItem
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TopBar() {
+fun TopBar(navController: NavHostController) {
     TopAppBar(
+        navigationIcon = {
+            val route = navController.currentBackStackEntryAsState().value?.destination?.route ?: ""
+            val isButton =
+                route != HomeNavigationItem.ResQ.route && route != ShareNavigationItem.Room.route && route != UserNavigationItem.User.route
+            IconButton(
+                onClick = { navController.popBackStack() },
+                colors = IconButtonDefaults.iconButtonColors(
+                    contentColor = Color.Black,
+                    disabledContentColor = Color.Transparent
+                ),
+                enabled = isButton
+            ) {
+                Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "ArrowBack")
+            }
+        },
         title = {
             Text(text = "ResQ")
         },
         actions = {
             IconButton(onClick = { /* Search Action */ }) {
-                Icon(Icons.Default.Search, contentDescription = "Search")
+                Icon(imageVector = Icons.Default.Search, contentDescription = "Search")
             }
         },
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview() {
+    TopBar(rememberNavController())
 }
