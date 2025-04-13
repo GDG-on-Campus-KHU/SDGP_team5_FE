d760f047167e6e9552c0e94ab9cf7d4bb9cc346f
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-04-14 00:56:01 +0900

    Add ResQDetail ViewModel example

--- app/src/main/java/com/example/resq/presentaion/resqdetail/ResQDetailViewModel.kt
+++ app/src/main/java/com/example/resq/presentaion/resqdetail/ResQDetailViewModel.kt
@@ -1,4 +1,30 @@
 package com.example.resq.presentaion.resqdetail
 
-class ResQDetailViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class ResQDetailViewModel : ViewModel() {
+
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading: StateFlow<Boolean> = _isLoading
+
+    private val _resQDetail = MutableStateFlow(emptyList<String>())
+    val resQDetail: StateFlow<List<String>> = _resQDetail
+
+    fun getResQDetail(resQ: String) {
+        viewModelScope.launch {
+            _isLoading.value = true
+            try {
+                delay(1000)
+                _resQDetail.value = listOf("resQ", "예시 resQDetail")
+            } catch (e: Exception) {
+
+            }
+            _isLoading.value = false
+        }
+    }
 }
