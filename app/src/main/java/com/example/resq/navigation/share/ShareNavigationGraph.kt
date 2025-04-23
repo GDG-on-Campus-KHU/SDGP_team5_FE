a0f20655c1eada02537c9e5c5da51f117ffc10ff
Taehoon Kim <124406982+taeh-kim@users.noreply.github.com>
2025-04-23 09:23:38 +0900

    Comment out undeveloped code

--- app/src/main/java/com/example/resq/navigation/share/ShareNavigationGraph.kt
+++ app/src/main/java/com/example/resq/navigation/share/ShareNavigationGraph.kt
@@ -6,16 +6,16 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.example.resq.presentaion.roomadd.RoomAddScreen
 import com.example.resq.presentaion.rooms.RoomsScreen
-import com.example.resq.presentaion.roomdetail.RoomDetailScreen
+//import com.example.resq.presentaion.roomdetail.RoomDetailScreen
 import com.example.resq.presentaion.roomnotify.RoomNotifyScreen
 
 fun NavGraphBuilder.shareNavigationGraph(navController: NavController, padding: PaddingValues) {
     composable(ShareNavigationItem.Rooms.route) {
         RoomsScreen(navController, padding)
     }
-    composable(ShareNavigationItem.RoomDetail.route) {
+    /*composable(ShareNavigationItem.RoomDetail.route) {
         RoomDetailScreen(navController, padding)
-    }
+    }*/
     composable(ShareNavigationItem.RoomAdd.route) {
         RoomAddScreen(navController, padding)
     }
