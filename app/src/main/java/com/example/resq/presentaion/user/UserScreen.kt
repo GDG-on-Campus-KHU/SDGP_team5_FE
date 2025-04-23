0df55c9fca569e57cda522da37fada35fffbcfa5
Taehoon Kim <124406982+taeh-kim@users.noreply.github.com>
2025-04-23 15:44:45 +0900

    Add Preview function

--- app/src/main/java/com/example/resq/presentaion/user/UserScreen.kt
+++ app/src/main/java/com/example/resq/presentaion/user/UserScreen.kt
@@ -6,7 +6,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun UserScreen(
@@ -20,4 +23,10 @@ fun UserScreen(
     ) {
 
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserScreenPreview() {
+    UserScreen(rememberNavController(), PaddingValues(0.dp))
 }
