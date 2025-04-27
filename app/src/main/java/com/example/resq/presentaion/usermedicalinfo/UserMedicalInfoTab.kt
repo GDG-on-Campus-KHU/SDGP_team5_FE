d803e084f11752f1420bc9bf2920062946b6ed74
Taehoon Kim <124406982+taeh-kim@users.noreply.github.com>
2025-04-27 23:17:20 +0900

    Add Frame of UpdateMedicalInfo

--- app/src/main/java/com/example/resq/presentaion/usermedicalinfo/UserMedicalInfoTab.kt
+++ app/src/main/java/com/example/resq/presentaion/usermedicalinfo/UserMedicalInfoTab.kt
@@ -9,10 +9,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material.icons.outlined.Language
@@ -26,12 +30,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.resq.R
 import com.example.resq.navigation.user.UserNavigationItem
+import com.example.resq.presentaion.user.UserScreen
 import com.example.resq.ui.theme.Gray2
 import com.example.resq.ui.theme.Gray4
 
@@ -45,6 +54,7 @@ fun UserMedicalInfoTab(
             .fillMaxSize()
             .padding(top = 5.dp, bottom = 10.dp, start = 5.dp, end = 5.dp)
             .background(Color.White, RoundedCornerShape(12.dp))
+            .verticalScroll(rememberScrollState())
     ) {
         val context = LocalContext.current
         val viewModel: UserMedicalInfoViewModel = viewModel()
@@ -71,7 +81,6 @@ fun UserMedicalInfoTab(
                     .size(28.dp)
                     .clickable {
                         isEditing.value = true
-                        tempName.value = viewModel.medicalInfoList.value.find { it.label == context.getString(R.string.info_name) }?.value?.value ?: ""
                     }
             )
         }
@@ -81,7 +90,13 @@ fun UserMedicalInfoTab(
             modifier = Modifier.padding(vertical = 7.dp, horizontal = 18.dp)
         )
 
-        viewModel.medicalInfoList.value.filter { it.show.value }.forEach { element ->
+        val itemsToDisplay = if (isEditing.value) {
+            viewModel.medicalInfoList.value
+        } else {
+            viewModel.medicalInfoList.value.filter { it.show.value }
+        }
+
+        itemsToDisplay.forEach { element ->
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -90,41 +105,64 @@ fun UserMedicalInfoTab(
                 Icon(
                     imageVector = element.icon,
                     contentDescription = null,
-                    modifier = Modifier.size(45.dp).padding(top = 7.dp)
+                    modifier = Modifier
+                        .size(45.dp)
+                        .padding(top = 7.dp)
                 )
                 Column(modifier = Modifier.padding(start = 16.dp)) {
-                    Text(text = element.label, fontSize = 12.sp)
-                    Text(text = element.value.value ?: element.placeholder, fontSize = 12.sp, color = Gray4)
+                    Text(
+                        text = element.label,
+                        fontSize = 12.sp
+                    )
+
+                    if (!isEditing.value) {
+                        Text(
+                            text = element.value.value ?: element.placeholder,
+                            fontSize = 12.sp,
+                            color = Gray4
+                        )
+                    } else {
+                        if (element.label in listOf("이름", "알레르기", "복용중인 약", "참고사항")) {
+                            BasicTextField(
+                                value = element.value.value ?: "",
+                                onValueChange = { element.value.value = it },
+                                textStyle = TextStyle(
+                                    fontSize = 12.sp,
+                                    color = Gray4
+                                ),
+                                modifier = Modifier.fillMaxWidth(),
+                                decorationBox = { innerTextField ->
+                                    if (element.value.value.isNullOrEmpty()) {
+                                        Text(
+                                            text = element.placeholder,
+                                            fontSize = 12.sp,
+                                            color = Gray4
+                                        )
+                                    }
+                                    innerTextField()
+                                }
+                            )
+                        } else {
+                            Text(
+                                text = element.value.value ?: element.placeholder,
+                                fontSize = 12.sp,
+                                color = Gray4,
+                                modifier = Modifier
+                                    .clickable {
+                                        // AllertDialog 함수 넣기
+                                    }
+                            )
+                        }
+                    }
                 }
             }
             Divider(modifier = Modifier.padding(vertical = 7.dp, horizontal = 25.dp))
         }
-
-        if (isEditing.value) {
-            TextField(
-                value = tempName.value,
-                onValueChange = { tempName.value = it },
-                label = { Text("이름 수정") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp, vertical = 8.dp)
-            )
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-            ) {
-                Button(
-                    onClick = {
-                        viewModel.medicalInfoList.value.find { it.label == context.getString(R.string.info_name) }?.value?.value = tempName.value
-                        isEditing.value = false
-                    },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text("저장")
-                }
-            }
-        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserMedicalInfoPreview() {
+    UserMedicalInfoTab(rememberNavController(), PaddingValues(0.dp))
 }
