c1a373d5d5d1bd01412eedb48a53373fbf174da7
GwonDongHyeon21 <GwonDH21@gmail.com>
2025-05-14 18:14:57 +0900

    Update UserTabBar design & translate icon

--- app/src/main/java/com/example/resq/presentaion/user/component/UserTabBar.kt
+++ app/src/main/java/com/example/resq/presentaion/user/component/UserTabBar.kt
@@ -1,20 +1,24 @@
 package com.example.resq.presentaion.user.component
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.resq.R
 import com.example.resq.ui.theme.Gray3
+import com.example.resq.ui.theme.InnerPadding
 import com.example.resq.ui.theme.MainBlack
 
 @Composable
@@ -22,13 +26,16 @@ fun UserTabBar(selectedTab: Int, onTabSelected: (Int) -> Unit) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(InnerPadding)
     ) {
-
-        Box(
+        Button(
             modifier = Modifier
                 .weight(1f)
-                .clickable { onTabSelected(0) },
-            contentAlignment = Alignment.Center
+                .border(1.dp, Color.Gray),
+            onClick = { onTabSelected(0) },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent
+            )
         ) {
             Text(
                 text = stringResource(R.string.medical_info),
@@ -37,12 +44,15 @@ fun UserTabBar(selectedTab: Int, onTabSelected: (Int) -> Unit) {
                 fontWeight = if (selectedTab == 0) FontWeight.Bold else FontWeight.Normal
             )
         }
-
-        Box(
+        Spacer(Modifier.width(10.dp))
+        Button(
             modifier = Modifier
                 .weight(1f)
-                .clickable { onTabSelected(1) },
-            contentAlignment = Alignment.Center
+                .border(1.dp, Color.Gray),
+            onClick = { onTabSelected(1) },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent
+            )
         ) {
             Text(
                 text = stringResource(R.string.record_list),
